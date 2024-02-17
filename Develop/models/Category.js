781e1c040652a114a640a5d896926d196411be55
Sean Harrigan <SeanHarrigan8@gmail.com>
2024-02-17 14:27:28 -0600

    seeded db, created models

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -6,15 +6,23 @@ class Category extends Model {}
 
 Category.init(
   {
-    // define columns
-  },
-  {
-    sequelize,
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+  }, {
+  
+  sequelize,
     timestamps: false,
     freezeTableName: true,
     underscored: true,
     modelName: 'category',
-  }
-);
+  });
 
 module.exports = Category;
